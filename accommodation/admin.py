30d46e1472bd174d95807223ab3f8b9cfa1747d4
Drew <ayobamioduola13@gmail.com>
2025-03-10 06:36:52 +0100

    feat(Accommodation): add basic accommodation endpoints"

--- accommodation/admin.py
+++ accommodation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Accommodation
 
 # Register your models here.
+@admin.register(Accommodation)
+class AccommodationAdmin(admin.ModelAdmin):
+    pass
