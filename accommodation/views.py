99e0bbc65be327beccc6c4c0933b24e71e187fc6
Drew <ayobamioduola13@gmail.com>
2025-03-13 16:59:38 +0100

    feat(accommodation): add seperate serializer for accommodation listing

--- accommodation/views.py
+++ accommodation/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import AccommodationSerializer
+from rest_framework.response import Response
+from .serializers import AccommodationSerializer, AccommodationListSerializer
 from .models import Accommodation
 
 # Create your views here.
 class AccommodationViewSet(ModelViewSet):
     queryset = Accommodation.objects.all()
     serializer_class = AccommodationSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = Accommodation.objects.all()
+        serializer = AccommodationListSerializer(queryset, many=True)
+        return Response(serializer.data)
