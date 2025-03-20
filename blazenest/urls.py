e4a2b08c17c93141e352cbee20346819ab7fa965
Drew <ayobamioduola13@gmail.com>
2025-03-20 17:24:32 +0100

    feat(booking): booking endpoints

--- blazenest/urls.py
+++ blazenest/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
     re_path(r'api/accommodation/', include('accommodation.urls')),
+    re_path(r'api/booking/', include('booking.urls'))
 ]
