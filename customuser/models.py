f516b241429ae3a180249b82889bef40468bab58
Drew <ayobamioduola13@gmail.com>
2025-03-21 14:44:22 +0100

    feat(vendors): create vendor app

--- customuser/models.py
+++ customuser/models.py
@@ -25,6 +25,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_vendor = models.BooleanField(default=False)
 
     objects = UserManager()
 
