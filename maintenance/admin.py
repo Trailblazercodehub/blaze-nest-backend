e6290f6551f2cb751b7123647894f66090d1657d
Drew <ayobamioduola13@gmail.com>
2025-04-30 15:55:46 +0100

    feat(maintenance): register maintenance model to admin

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MaintenanceReport
 
 # Register your models here.
+@admin.register(MaintenanceReport)
+class MaintenanceReportAdmin(admin.ModelAdmin):
+    list_display = ('user', 'accommondation', 'description')
+    search_fields = ('user__username', 'accommondation__name', 'description')
+    list_filter = ('user', 'accommondation')
+    
