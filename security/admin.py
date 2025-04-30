33559db934322d03dc27aebb6272e87b9ccf6b10
Drew <ayobamioduola13@gmail.com>
2025-04-30 15:59:54 +0100

    feat(vendors): register vender model to admin

--- security/admin.py
+++ security/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import SecurityReport
 
 # Register your models here.
+@admin.register(SecurityReport)
 class SecurityReportAdmin(admin.ModelAdmin):
     list_display = ('user', 'accommondation', 'description')
     search_fields = ('user__username', 'accommondation__name', 'description')
