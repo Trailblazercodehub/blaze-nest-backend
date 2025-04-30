19ec6e7a5f3283bd6246b4605c6971923ed8314a
Drew <ayobamioduola13@gmail.com>
2025-04-30 15:58:00 +0100

    feat(security): security report

--- security/admin.py
+++ security/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import SecurityReport
 
 # Register your models here.
+class SecurityReportAdmin(admin.ModelAdmin):
+    list_display = ('user', 'accommondation', 'description')
+    search_fields = ('user__username', 'accommondation__name', 'description')
+    list_filter = ('user', 'accommondation')
+    
+    
