811de220a29ebb7daaf20cd9202bde10d56c03a1
Drew <ayobamioduola13@gmail.com>
2025-04-30 15:59:54 +0100

    feat(vendors): register vender model to admin

--- vendors/admin.py
+++ vendors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Vendor
 
 # Register your models here.
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('business_name', 'email', 'is_verified', 'date_joined')
+    search_fields = ('business_name', 'email')
+    list_filter = ('is_verified', 'date_joined')
+    ordering = ('-date_joined',)
+    list_per_page = 20
