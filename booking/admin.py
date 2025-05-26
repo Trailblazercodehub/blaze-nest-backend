bf521c881238c4f78b3ccfcb3e14e59e21921941
NathanielWatife <awosanminathaniel0@gmail.com>
2025-05-26 14:10:53 +0100

    changes made to booking/admin

--- booking/admin.py
+++ booking/admin.py
@@ -4,6 +4,6 @@ from .models import Booking
 # Register your models here.
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('user', 'accommondation')
+    list_display = ('user', 'accommodation')
     search_fields = ('user__username', 'accommondation__name')
     
